2b3e7361b8cd7a0b7b479af401982ecbfdf1196f
Vernon Young <Vernon Young>
2016-07-22 09:17:00 -0500

    Update - Tweeking function code

--- src/app/pages/tools/disable/disableCtrl.js
+++ src/app/pages/tools/disable/disableCtrl.js
@@ -10,10 +10,16 @@
 
   /** @ngInject */
   function DisableCtrl($scope) {
-    $scope.default = {opt_Account: "renamed_admin", chk_Account: true};
+    $scope.default = {opt_Account: "renamed_admin", chk_Account: true, txt_Console: "Console output will appear here..."};
     $scope.reset = function () {
       $scope.current = angular.copy($scope.default);
     };
     $scope.reset();
+    $scope.console_clear = function () {
+      $scope.current.txt_Console = angular.copy($scope.default.txt_Console);
+    };
+    $scope.console_test = function () {
+      $scope.current.txt_Console = "Testing 123";
+    };
   }
 })();
